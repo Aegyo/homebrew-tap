6618a009a8bb7945e5d4b75108006f9ab3b1e6d5
Jessica Chen <jessica.scoltock@protonmail.com>
2023-05-13 02:01:53 +0100

    Update sha256

--- Casks/pokeclicker-desktop.rb
+++ Casks/pokeclicker-desktop.rb
@@ -1,6 +1,6 @@
 cask "pokeclicker-desktop" do
   version "1.2.1-alpha"
-  sha256 "tmp"
+  sha256 "248aa505135ed14744e1286a01860797425ef87d0c84a6c14cd838571f99a7d4"
   url "https://github.com/Aegyo/Pokeclicker-desktop/releases/download/v#{version}/PokeClicker-#{version}-mac.zip"
   name "Pokeclicker Desktop"
   desc "Electron desktop client for Pokeclicker"
